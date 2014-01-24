8e037bbfe217660f32f70a40a8105ad5dafa89aa
zaccary price <zaccary.price@avansera.com>
2014-01-24 13:33:27 +0200

    fix graph controller something something

--- app/scripts/controllers/graphcontroller.js
+++ app/scripts/controllers/graphcontroller.js
@@ -4,37 +4,42 @@
 
 angular.module('Deep.Controllers')
 	.controller('GraphCtrl', function($scope, $rootScope, $routeParams, $graph, $log) {
-		var graphHash = $routeParams.graph;
+		var graphHash = $routeParams.graph || false;
 
 		$scope.errors = [];
 		$scope.successes = [];
 		$scope.graphData = null;
 
-        // get the data for the selected graph
-		$graph.getGraphList(graphHash)
-			.then(
-                function(success) {
+        if(!!graphHash) {
 
-                    // generate graph data using the graph data obj
-                    $graph.generateGraph(success[0])
-                        .then(
-                            function(success) {
-                                $log.info('DEBUG:', "Got the graph data and added to scope", success);
-                                $scope.graphData = success;
-                            },
-                            function(reason) {
-                                $log.warn('ERR:', "generating graph failed because", reason);
-                                $scope.errors.push(reason);
-                            }
-                        );
+            // get the data for the selected graph
+            $graph.getGraphList(graphHash)
+                .then(
+                    function(success) {
 
-                    //for(var g=0;g<graphs)
+                        // generate graph data using the graph data obj
+                        $graph.generateGraph(success[0])
+                            .then(
+                                function(success) {
+                                    $log.info('DEBUG:', "Got the graph data and added to scope", success);
+                                    $scope.graphData = success;
+                                },
+                                function(reason) {
+                                    $log.warn('ERR:', "generating graph failed because", reason);
+                                    $scope.errors.push(reason);
+                                }
+                            );
+
+                        //for(var g=0;g<graphs)
 
 
-                },
-                function(reason) {
-                    $log.warn('GEN GRAPH:', "something went wrong getting graph data", reason);
-                    $scope.errors.push(reason);
-                }
-		    );
+                    },
+                    function(reason) {
+                        $log.warn('GEN GRAPH:', "something went wrong getting graph data", reason);
+                        $scope.errors.push(reason);
+                    }
+                );
+        } else {
+            $scope.errors.push("no graph")
+        }
 	});
